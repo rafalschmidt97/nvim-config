6ca29f8482266f9f99e23aa01fab16c38e3fc853
Rafał Schmidt <rafal.schmidt@allegro.com>
2024-03-18 19:55:01 +0100

    feat: use extras

--- lua/plugins/csharp-lsp.lua
+++ lua/plugins/csharp-lsp.lua
@@ -1,29 +1,30 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "c_sharp",
-      })
-    end,
-  },
-
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "csharp-language-server",
-        "csharpier",
-      })
-    end,
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        csharp_ls = {},
-      },
-    },
-  },
+  { import = "lazyvim.plugins.extras.lang.omnisharp" },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "c_sharp",
+  --     })
+  --   end,
+  -- },
+  --
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "csharp-language-server",
+  --       "csharpier",
+  --     })
+  --   end,
+  -- },
+  --
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       csharp_ls = {},
+  --     },
+  --   },
+  -- },
 }
--- lua/plugins/json-lsp.lua
+++ lua/plugins/json-lsp.lua
@@ -1,28 +1,29 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "json",
-      })
-    end,
-  },
-
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "json-lsp",
-      })
-    end,
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        jsonls = {},
-      },
-    },
-  },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "json",
+  --     })
+  --   end,
+  -- },
+  --
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "json-lsp",
+  --     })
+  --   end,
+  -- },
+  --
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       jsonls = {},
+  --     },
+  --   },
+  -- },
 }
