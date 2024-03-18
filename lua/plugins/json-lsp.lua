531331a745638bc2a1f365a7d7e9464b6b3106f3
Rafał Schmidt <rafal.schmidt@allegro.com>
2024-03-18 19:01:09 +0100

    feat: support json

---
+++ lua/plugins/json-lsp.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "json",
+      })
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "json-lsp",
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        jsonls = {},
+      },
+    },
+  },
+}
