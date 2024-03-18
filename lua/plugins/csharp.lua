915cfd772357cfd825dd5d48bba6c5078050c190
Rafał Schmidt <rafal.schmidt@allegro.com>
2024-03-18 14:17:13 +0100

    feat: csharp with nvim and lazyvim

---
+++ lua/plugins/csharp.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "c_sharp",
+      })
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "csharp-language-server",
+        "csharpier",
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        csharp_ls = {},
+      },
+    },
+  },
+}
