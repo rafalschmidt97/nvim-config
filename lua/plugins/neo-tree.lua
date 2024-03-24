a01292407b47f89428731d52c352fb91743b4a05
Rafał Schmidt <rafalschmidt@Rafas-MacBook-Pro.local>
2024-03-24 15:45:57 +0100

    feat: show hidden

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          --visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {},
+          never_show = { ".git" },
+        },
+      },
+    },
+  },
+}
