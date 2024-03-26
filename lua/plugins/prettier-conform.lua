c8941ddc817510b27b56d4abc61c9e66a7b3c11d
Rafał Schmidt <rafal.schmidt@allegro.com>
2024-03-26 13:23:23 +0100

    fix: disable format on save by type

--- lua/plugins/prettier-conform.lua
+++ lua/plugins/prettier-conform.lua
@@ -13,8 +13,7 @@ return {
         ["css"] = { "prettier" },
         ["scss"] = { "prettier" },
         ["less"] = { "prettier" },
-        ["html"] = {}, -- disable for htmx and go templates
-        -- ["html"] = { "prettier" },
+        ["html"] = { "prettier" },
         ["json"] = { "prettier" },
         ["jsonc"] = { "prettier" },
         ["yaml"] = { "prettier" },
@@ -23,6 +22,23 @@ return {
         ["graphql"] = { "prettier" },
         ["handlebars"] = { "prettier" },
       },
+      format_on_save = function(bufnr)
+        -- Disable autoformat on certain filetypes
+        local ignore_filetypes = { "html", "yaml" }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        -- Disable autoformat for files in a certain path
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match("/node_modules/") then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
     },
   },
 }
