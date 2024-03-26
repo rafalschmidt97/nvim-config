5b2773ae627ecb9318b483208bfed097be648097
Rafał Schmidt <rafalschmidt@Rafas-MacBook-Pro.local>
2024-03-26 08:56:30 +0100

    fix: disable prettier for htmx html

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.tabstop = 4 -- Number of spaces tabs count for, 4 for golang
+local opt = vim.opt
+
+-- opt.tabstop = 4 -- Number of spaces tabs count for, 4 for golang
+-- opt.shiftwidth = 4 -- Size of an indent
+-- opt.expandtab = false -- Use spaces instead of tabs
--- lua/plugins/prettier-conform.lua
+++ lua/plugins/prettier-conform.lua
@@ -1,3 +1,28 @@
 return {
   { import = "lazyvim.plugins.extras.formatting.prettier" },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = { "prettier" },
+        ["javascriptreact"] = { "prettier" },
+        ["typescript"] = { "prettier" },
+        ["typescriptreact"] = { "prettier" },
+        ["vue"] = { "prettier" },
+        ["css"] = { "prettier" },
+        ["scss"] = { "prettier" },
+        ["less"] = { "prettier" },
+        ["html"] = {}, -- disable for htmx and go templates
+        -- ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
+        ["handlebars"] = { "prettier" },
+      },
+    },
+  },
 }
