cdf2850aecd1ec9716e3db33e4993bce8148ccd0
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-03-29 09:44:01 +0100

    feat: use lazy extras

--- lua/plugins/formatter/prettier-conform-formatter.lua
+++ lua/plugins/formatter/prettier-conform-formatter.lua
@@ -6,21 +6,14 @@ return {
     opts = {
       formatters_by_ft = {
         ["javascript"] = { "prettier" },
-        ["javascriptreact"] = { "prettier" },
         ["typescript"] = { "prettier" },
-        ["typescriptreact"] = { "prettier" },
-        ["vue"] = { "prettier" },
         ["css"] = { "prettier" },
-        ["scss"] = { "prettier" },
-        ["less"] = { "prettier" },
         ["html"] = { "prettier" },
         ["json"] = { "prettier" },
         ["jsonc"] = { "prettier" },
         ["yaml"] = { "prettier" },
         ["markdown"] = { "prettier" },
         ["markdown.mdx"] = { "prettier" },
-        ["graphql"] = { "prettier" },
-        ["handlebars"] = { "prettier" },
       },
       -- format_on_save = function(bufnr)
       --   -- Disable autoformat on certain filetypes
--- lua/plugins/lsp-autosave.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- autoformat for specific files is disable in lua/config/autocmds.lua
-    },
-  },
-}
