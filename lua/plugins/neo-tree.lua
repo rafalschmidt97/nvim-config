f9608e135f557fc7f20e94dbda4b3bff23ac375f
Rafał Schmidt <rafalschmidt97@gmail.com>
2024-10-06 18:08:10 +0200

    feat: wider neotree, copilot test

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,6 +11,30 @@ return {
           never_show = { ".git" },
         },
       },
+      window = {
+        width = 60,
+      },
+      default_component_configs = {
+        file_size = {
+          enabled = false,
+          required_width = 84,
+        },
+        type = {
+          enabled = false,
+          required_width = 142,
+        },
+        last_modified = {
+          enabled = false,
+          required_width = 108,
+        },
+        created = {
+          enabled = false,
+          required_width = 130,
+        },
+        symlink_target = {
+          enabled = false,
+        },
+      },
       event_handlers = {
         {
           event = "neo_tree_buffer_enter",
