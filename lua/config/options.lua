2287d6a3a63d3d6cdf457cf3b96229804dd0f6f5
Rafał Schmidt <rafal.schmidt@allegro.com>
2025-02-12 16:54:08 +0100

    fix: use clipboard and do not overide to blackhole

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,8 @@ local opt = vim.opt
 
 -- opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
 opt.conceallevel = 0
+
+-- If you want your clipboard to remain unchanged when replacing text, you can enable system clipboard:
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+-- vnoremap p "0p -- 0 stores the last yanked text even after deleting:
+vim.keymap.set("v", "p", '"_dP', { noremap = true, silent = true }) -- black hole register discards deleted text
