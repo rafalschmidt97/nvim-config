318a318e78ddd912742d2ca08488ad37848eacbb
Rafał Schmidt <rafal.schmidt@allegro.com>
2025-11-21 08:58:14 +0100

    feat: color scheme dynamic

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,19 @@
+-- dynamic color scheme
+---@diagnostic disable-next-line: lowercase-global
+function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    -- return "Builtin Solarized Dark"
+    -- return "Gruvbox Dark (Gogh)"
+    return "darcula-solid" -- JetBrains Darcula equivalent
+  else
+    -- return "Gruvbox light, hard (base16)"
+    -- return "GruvboxLight"
+    return "gruvbox" -- Gruvbox (Gogh) equivalent
+    -- return "Builtin Solarized Light"
+  end
+end
+
 return {
-  -- { "ellisonleao/gruvbox.nvim" },
   {
     "briones-gabriel/darcula-solid.nvim",
     dependencies = {
@@ -8,18 +22,32 @@ return {
   },
 
   {
-    "maxmx03/solarized.nvim",
-  },
-
-  {
     "ellisonleao/gruvbox.nvim",
+    config = function()
+      require("gruvbox").setup({
+        contrast = "", -- can be "hard", "soft" or empty string
+      })
+    end,
   },
 
   {
     "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "gruvbox",
-      colorscheme = "darcula-solid",
-    },
+    opts = function()
+      -- Detect system appearance (dark/light mode) to match wezterm
+      local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+      local appearance = "Light"
+      
+      if handle then
+        local result = handle:read("*a")
+        handle:close()
+        if result:find("Dark") then
+          appearance = "Dark"
+        end
+      end
+      
+      return {
+        colorscheme = scheme_for_appearance(appearance),
+      }
+    end,
   },
 }
